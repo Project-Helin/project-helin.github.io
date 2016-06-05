f9ec24d8fe0c0e99e13ec9f89fc4fc7decc77230
Marcel Amsler <marcel.amsler@gmail.com>
2016-06-05 16:03:49 +0200

    finished language feature

--- js/language-initializer.js
+++ js/language-initializer.js
@@ -1,17 +1,27 @@
-$( document ).ready( function( $ ) {
-  var userLang = navigator.language || navigator.userLanguage;
-
-  console.log(userLang);
+function updateLanguage(reload) {
+  var userLang = window.language || navigator.language || navigator.userLanguage;
+  var langCode = userLang.substr(0, 2);
+  var defaultLanguage = "en";
 
   var i18n = $.i18n();
-  i18n.locale = "de";
+  i18n.locale = langCode;
 
-  var language = $('.language option:selected').val();
+  if (i18n.locale != defaultLanguage) {
+    i18n.load('i18n/' + i18n.locale + '.json', i18n.locale).done(function () {
+      $('[data-i18n]').i18n()
+    });
+  } else if (reload) {
+    location.reload();
+  }
+}
 
-  i18n.load('i18n/' + i18n.locale + '.json', i18n.locale).done(function() {
-    $('[data-i18n]').i18n()
-  });
-
-  i18n.debug = true;
+$( document ).ready( function() {
+  updateLanguage();
 
 });
+
+function languageChosen (event, lang) {
+  event.preventDefault();
+  window.language = lang;
+  updateLanguage(true);
+}
--- js/main.js
+++ js/main.js
@@ -19,11 +19,13 @@ jQuery(function($) {
 
 	//smooth scroll
 	$('.navbar-nav > li').click(function(event) {
-		event.preventDefault();
 		var target = $(this).find('>a').prop('hash');
-		$('html, body').animate({
-			scrollTop: $(target).offset().top
-		}, 500);
+		if (target.offset()) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: $(target).offset().top
+			}, 500);
+		}
 	});
 
 	//scrollspy
