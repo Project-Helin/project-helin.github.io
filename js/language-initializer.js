5a608eed4f5a49b077339420d269bd16c63f6800
Marcel Amsler <marcel.amsler@gmail.com>
2016-06-05 16:23:41 +0200

    fixed problem with not supported languages

--- js/language-initializer.js
+++ js/language-initializer.js
@@ -3,24 +3,35 @@ function updateLanguage(reload) {
   var langCode = userLang.substr(0, 2);
   var defaultLanguage = "en";
 
-  var i18n = $.i18n();
-  i18n.locale = langCode;
 
-  if (i18n.locale != defaultLanguage) {
-    i18n.load('i18n/' + i18n.locale + '.json', i18n.locale).done(function () {
-      $('[data-i18n]').i18n()
-    });
-  } else if (reload) {
-    location.reload();
+  var supportedLanguages = ["en", "de"];
+
+  var i18n = $.i18n();
+
+  var languageSupported = supportedLanguages.filter(function (supportedLang) {
+    return supportedLang === langCode;
+  })[0];
+
+  if (languageSupported) {
+    i18n.locale = langCode;
+    console.log(langCode);
+
+    if (i18n.locale != defaultLanguage) {
+      i18n.load('i18n/' + i18n.locale + '.json', i18n.locale).done(function () {
+        $('[data-i18n]').i18n()
+      });
+    } else if (reload) {
+      location.reload();
+    }
   }
 }
 
-$( document ).ready( function() {
+$(document).ready(function () {
   updateLanguage();
 
 });
 
-function languageChosen (event, lang) {
+function languageChosen(event, lang) {
   event.preventDefault();
   window.language = lang;
   updateLanguage(true);
