1dc1fe909d3de5e3e4c500bfe0d362d3ee0cb83a
Marcel Amsler <marcel.amsler@gmail.com>
2016-06-07 22:01:30 +0200

    fixed language initializer

--- js/language-initializer.js
+++ js/language-initializer.js
@@ -1,37 +1,39 @@
 function updateLanguage(reload) {
-  var userLang = window.language || navigator.language || navigator.userLanguage;
-  var langCode = userLang.substr(0, 2);
-  var defaultLanguage = "en";
-  
+  var langCode = document.location.hash.substr(1, 3) || navigator.language.substr(0, 2) || navigator.userLanguage.substr(0, 2);
   var supportedLanguages = ["en", "de"];
+  var defaultLanguage = "en";
 
-  var i18n = $.i18n();
 
   var languageSupported = supportedLanguages.filter(function (supportedLang) {
     return supportedLang === langCode;
   })[0];
 
-  if (languageSupported) {
-    i18n.locale = langCode;
-    console.log(langCode);
-
-    if (i18n.locale != defaultLanguage) {
-      i18n.load('i18n/' + i18n.locale + '.json', i18n.locale).done(function () {
-        $('[data-i18n]').i18n()
-      });
-    } else if (reload) {
-      location.reload();
-    }
+  if (!languageSupported) {
+    langCode = defaultLanguage;
   }
+
+  //write lang to url in order to preserve it after reload
+  document.location.hash = langCode;
+
+  var i18n = $.i18n();
+  i18n.locale = langCode;
+
+  if (i18n.locale != defaultLanguage) {
+    i18n.load('i18n/' + i18n.locale + '.json', i18n.locale).done(function () {
+      $('[data-i18n]').i18n()
+    });
+  } else if (reload) {
+    location.reload();
+  }
+
 }
 
 $(document).ready(function () {
   updateLanguage();
-
 });
 
 function languageChosen(event, lang) {
   event.preventDefault();
-  window.language = lang;
+  document.location.hash = lang;
   updateLanguage(true);
 }
